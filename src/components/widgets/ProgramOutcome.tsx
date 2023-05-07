362520aad97296fa538210a8cac89d9cbd1272d7
Abdullah Ijaz <52850964+Abdullahijazvu@users.noreply.github.com>
2023-05-08 02:02:26 +0500

    Commit

--- src/components/widgets/ProgramOutcome.tsx
+++ src/components/widgets/ProgramOutcome.tsx
@@ -14,7 +14,7 @@ const ProgramOutcome = () => {
             </div>
             <div className="flex-1">
                 <h2 className="font-bold text-3xl lg:text-6xl">The Outcome for Participants of the Program</h2>
-                <p className="mt-4 text-slate-600 text-lg">As a graduate of this program, you will own valuable products such as Full-Stack App Templates, AR and VR Experiences, and APIs that are marketed globally by the Panaverse DAO. You will also have the opportunity to offer your services at a rate of $50 per hour, providing a path to financial stability while contributing to the growth of Pakistan's software exports.</p>
+                <p className="mt-4 text-slate-600 text-lg">As a graduate of this program, you will own valuable products such as Full-Stack App Templates, AR and VR Experiences, and APIs that are marketed globally by the Panaverse DAO. You will also have the opportunity to offer your services at a rate of $50 per hour, providing a path to financial stability while contributing to the growth of Pakistan&rsquo;s software exports.</p>
                 <div className="mt-6 grid grid-row sm:grid-cols-2 gap-x-2 gap-y-4">
                     {
                         outcomePoints.map((item, i)=>(
